83ff25ff68f03f2d783e3ef939b4236127f2e905
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-10-22 20:42:39 +0100

    Add CBV alternative to download_csv view

--- django_csv/views.py
+++ django_csv/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db.models.query import QuerySet
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.views import View
 
 from .csv import write_csv
 from .models import CsvDownload
@@ -21,3 +22,49 @@ def download_csv(
         columns=", ".join(columns),
     )
     return response
+
+
+class DownloadForbidden(Exception):
+    pass
+
+
+class DownloadCsv(View):
+    """CBV for downloading CSVs."""
+
+    # list of columns to be extracted from the queryset
+    csv_columns = []
+
+    def authorize(self, request: HttpRequest) -> None:
+        """
+        Authorize the download request.
+
+        Raise DownloadForbidden if download is not permitted.
+
+        Default is to allow all authenticated users access.
+
+        """
+        if not request.user.is_authenticated:
+            raise DownloadForbidden()
+
+    def filename(self, request: HttpRequest) -> str:
+        """Return download filename."""
+        raise NotImplementedError()
+
+    def queryset(self, request: HttpRequest) -> QuerySet:
+        """Fetch the appropriate data for the user."""
+        raise NotImplementedError()
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        """Download bookings as CSV."""
+        if not self.csv_columns:
+            raise ValueError("DownloadCsv subclass must specify csv_columns")
+        try:
+            self.authorize(request)
+        except DownloadForbidden:
+            return HttpResponseForbidden()
+        return download_csv(
+            request.user,
+            self.filename(),
+            self.queryset(request),
+            *self.csv_columns
+        )
