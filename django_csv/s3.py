ced5d294f849abcd3e25a5cfe49ff4898cd32b5c
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-03-15 20:02:25 +0000

    Add support for multi-part uploads to S3

--- django_csv/s3.py
+++ django_csv/s3.py
@@ -9,29 +9,45 @@ except ImportError:
     raise ImportError("You cannot use the django_csv.s3 module without boto3.")
 
 
-def _put_object(bucket: str, key: str, body: BytesIO) -> None:
+# extracted out to facilitate testing
+def _put_object(bucket: str, key: str, fileobj: BytesIO) -> None:
     """Upload binary stream to S3 using put_pubject."""
     client = boto3.client("s3")
-    client.put_object(Bucket=bucket, Key=key, Body=body, ContentType="text/csv")
+    client.put_object(Bucket=bucket, Key=key, Body=fileobj, ContentType="text/csv")
+
+
+# extracted out to facilitate testing
+def _upload_fileobj(bucket: str, key: str, fileobj: BytesIO) -> None:
+    """Upload binary stream to S3 using upload_fileobj."""
+    client = boto3.client("s3")
+    client.upload_fileobj(fileobj, bucket, key)
 
 
 @contextlib.contextmanager
-def s3_stream(bucket: str, key: str) -> Generator:
+def s3_stream(bucket: str, key: str, multipart: bool = False) -> Generator:
     """
     Return a context manager used to write csv to S3.
 
-    This is a wrapper that provides access to a BytesIO stream (that
-    boto3 can use to push a file to S3) via a TextIO wrapper that
-    csv.writer can use.
+    This is a wrapper that provides access to a BytesIO stream (that boto3 can
+    use to push a file to S3) via a TextIO wrapper that csv.writer can use. If
+
+    multipart arg is True this uses the upload_fileobj boto3 function rather
+    than put_object (default). This may be a better option for large files.
 
     >>> with s3_stream("bucket", "obj_key") as fileobj:
     ...   write_csv(fileobj, queryset, "col1", "col2")
 
     """
-    with BytesIO() as binary_stream:
+    with BytesIO() as fileobj:
         with TextIOWrapper(
-            binary_stream, encoding="utf-8", newline="", write_through=True
-        ) as text_stream:
-            yield text_stream
-            binary_stream.seek(0)
-            _put_object(bucket, key, binary_stream)
+            fileobj,
+            encoding="utf-8",
+            newline="",
+            write_through=True,
+        ) as buffer:
+            yield buffer
+            fileobj.seek(0)
+            if multipart:
+                _upload_fileobj(bucket, key, fileobj)
+            else:
+                _put_object(bucket, key, fileobj)
