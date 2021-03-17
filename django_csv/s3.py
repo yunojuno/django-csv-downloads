ee84005787f4d6825faa75de476d528d5401201a
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-03-17 18:48:24 +0000

    Convert s3_stream to use TemporaryFile rather than BytesIO
    Uploading a big file has caused heroku to choke, so trying to reduce
    memory.

--- django_csv/s3.py
+++ django_csv/s3.py
@@ -1,53 +1,71 @@
 """Optional functions for uploading data direct to S3."""
 import contextlib
 from io import BytesIO, TextIOWrapper
-from typing import Generator
+from tempfile import TemporaryFile
+from typing import IO, Generator, Union
 
 try:
     import boto3
 except ImportError:
     raise ImportError("You cannot use the django_csv.s3 module without boto3.")
 
+# type used to smooth over TemporaryFile <> BytesIO mismatch
+FileLikeObject = Union[IO[bytes], BytesIO]
+
 
 # extracted out to facilitate testing
-def _put_object(bucket: str, key: str, fileobj: BytesIO) -> None:
+def _put_object(bucket: str, key: str, fileobj: FileLikeObject) -> None:
     """Upload binary stream to S3 using put_pubject."""
     client = boto3.client("s3")
     client.put_object(Bucket=bucket, Key=key, Body=fileobj, ContentType="text/csv")
 
 
 # extracted out to facilitate testing
-def _upload_fileobj(bucket: str, key: str, fileobj: BytesIO) -> None:
+def _upload_fileobj(bucket: str, key: str, fileobj: FileLikeObject) -> None:
     """Upload binary stream to S3 using upload_fileobj."""
     client = boto3.client("s3")
     client.upload_fileobj(fileobj, bucket, key)
 
 
 @contextlib.contextmanager
-def s3_stream(bucket: str, key: str, multipart: bool = False) -> Generator:
+def s3_upload_fileobj(bucket: str, key: str) -> Generator:
     """
-    Return a context manager used to write csv to S3.
+    Context manager used to write to S3 using upload_fileobj.
 
-    This is a wrapper that provides access to a BytesIO stream (that boto3 can
-    use to push a file to S3) via a TextIO wrapper that csv.writer can use. If
+    This context manager writes to a TemporaryFile and then uses the
+    multipart boto3 upload function `upload_fileobj`, and is more
+    appropriate for large files.
 
-    multipart arg is True this uses the upload_fileobj boto3 function rather
-    than put_object (default). This may be a better option for large files.
+    >>> with s3_upload_fileobj("bucket", "obj_key") as fileobj:
+    ...     write_csv(fileobj, queryset, "col1", "col2")
 
-    >>> with s3_stream("bucket", "obj_key") as fileobj:
-    ...   write_csv(fileobj, queryset, "col1", "col2")
+    """
+    with TemporaryFile() as fileobj:
+        with TextIOWrapper(
+            fileobj, encoding="utf-8", newline="", write_through=True
+        ) as buffer:
+            yield buffer
+            fileobj.seek(0)
+            _upload_fileobj(bucket, key, fileobj)
+
+
+@contextlib.contextmanager
+def s3_put_object(bucket: str, key: str) -> Generator:
+    """
+    Context manager used to write to S3 using put_object.
+
+    This context manager writes to a in-memory buffer and then uses the
+    one-shot boto3 upload function `put_object`, and is more appropriate
+    for smaller files.
+
+    >>> with s3_put_object("bucket", "obj_key") as fileobj:
+    ...     write_csv(fileobj, queryset, "col1", "col2")
 
     """
     with BytesIO() as fileobj:
         with TextIOWrapper(
-            fileobj,
-            encoding="utf-8",
-            newline="",
-            write_through=True,
+            fileobj, encoding="utf-8", newline="", write_through=True
         ) as buffer:
             yield buffer
             fileobj.seek(0)
-            if multipart:
-                _upload_fileobj(bucket, key, fileobj)
-            else:
-                _put_object(bucket, key, fileobj)
+            _put_object(bucket, key, fileobj)
--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,30 +1,37 @@
-from io import BytesIO
+from io import BufferedIOBase
 from unittest import mock
 
 import pytest
-from botocore.client import BaseClient
 from django.contrib.auth.models import User
 
 from django_csv import csv, s3
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize(
-    "multipart,callable",
-    [
-        (True, "django_csv.s3._upload_fileobj"),
-        (False, "django_csv.s3._put_object"),
-    ],
-)
-def test_s3_stream(multipart, callable):
+@mock.patch("django_csv.s3._upload_fileobj")
+def test_upload_fileobj(mock_upload):
     columns = ("first_name", "last_name")
     User.objects.create_user("user1")
-    with mock.patch(callable) as mock_upload:
-        with s3.s3_stream("bucket_name", "filename", multipart=multipart) as fileobj:
-            row_count = csv.write_csv(fileobj, User.objects.all(), *columns)
-            assert row_count == 1
-        assert mock_upload.call_count == 1
-        call_args = mock_upload.call_args[0]
-        assert call_args[0] == "bucket_name"
-        assert call_args[1] == "filename"
-        assert isinstance(call_args[2], BytesIO)
+    with s3.s3_upload_fileobj("bucket_name", "filename") as fileobj:
+        row_count = csv.write_csv(fileobj, User.objects.all(), *columns)
+        assert row_count == 1
+    assert mock_upload.call_count == 1
+    call_args = mock_upload.call_args[0]
+    assert call_args[0] == "bucket_name"
+    assert call_args[1] == "filename"
+    assert isinstance(call_args[2], BufferedIOBase)
+
+
+@pytest.mark.django_db
+@mock.patch("django_csv.s3._put_object")
+def test_s3_put_object(mock_upload):
+    columns = ("first_name", "last_name")
+    User.objects.create_user("user1")
+    with s3.s3_put_object("bucket_name", "filename") as fileobj:
+        row_count = csv.write_csv(fileobj, User.objects.all(), *columns)
+        assert row_count == 1
+    assert mock_upload.call_count == 1
+    call_args = mock_upload.call_args[0]
+    assert call_args[0] == "bucket_name"
+    assert call_args[1] == "filename"
+    assert isinstance(call_args[2], BufferedIOBase)
